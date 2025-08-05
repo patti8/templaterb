56268195679e8656227e0948df639aeca813dd37
tndyrvld <pattipeilohyterry@gmail.com>
2025-08-05 23:32:27 +0800

    Initial commit: Setup Rails app with Devise, OmniAuth, and Stripe

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Setup Devise modules, termasuk omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  # Callback method untuk OmniAuth
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      # user.name = auth.info.name   # asumsikan Anda memiliki kolom nama
+      # user.image = auth.info.image # asumsikan Anda memiliki kolom gambar
+    end
+  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+# Konfigurasi Stripe
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PUBLIC_KEY'],
+  secret_key:      ENV['STRIPE_SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
